7a058564ce5411bcaff7a8cf0c244bffcaae6467
Riyan <griyan115@gmail.com>
2025-03-25 22:55:20 +0530

    added download

--- src/my-resume/[resumeId]/view/viewResume.jsx
+++ src/my-resume/[resumeId]/view/viewResume.jsx
@@ -8,11 +8,11 @@ import { useParams } from 'react-router-dom'
 
 function ViewResume() {
     const [resumeInfo, setResumeInfo] = useState();
-    const {resumeId} = useParams();
+    const { resumeId } = useParams();
 
-    useEffect(()=>{
+    useEffect(() => {
         GetResumeInfo()
-    },[])
+    }, [])
 
     const GetResumeInfo = () => {
         GlobalApi.GetResumeById(resumeId).then(resp => {
@@ -20,18 +20,25 @@ function ViewResume() {
             setResumeInfo(resp.data.data)
         })
     }
+    const handleDownload = () => {
+        window.print();
+    }
 
     return (
         <ResumeInfoContext.Provider value={{ resumeInfo, setResumeInfo }}>
-            <Header />
-            <div className='my-10 mx-10 md:mx-20 lg:mx-36'>
-                <h2 className='text-center text-2xl font-medium'>Congrats! Your ultimate AI generated resume is ready!</h2>
-                <p className='text-center text-gray-400'>Now you are ready to download your resume and you can share your unique resume url with your friends and family</p>
-                <div className='flex  justify-between my-10 px-30'>
-                    <Button>Download</Button>
-                    <Button>Share</Button>
+            <div id='noPrintArea'>
+                <Header />
+                <div className='my-10 mx-10 md:mx-20 lg:mx-36'>
+                    <h2 className='text-center text-2xl font-medium'>Congrats! Your ultimate AI generated resume is ready!</h2>
+                    <p className='text-center text-gray-400'>Now you are ready to download your resume and you can share your unique resume url with your friends and family</p>
+                    <div className='flex  justify-between my-10 px-30'>
+                        <Button onClick={handleDownload}>Download</Button>
+                        <Button>Share</Button>
+                    </div>
                 </div>
-                <div>
+            </div>
+            <div className='my-10 md:mx-20 lg:mx-36'>
+                <div id='printArea'>
                     <ResumePreviewSection />
                 </div>
             </div>
