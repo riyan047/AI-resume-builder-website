657908e88a242f4582bd80a308f7a4d1434961aa
Riyan <griyan115@gmail.com>
2025-05-06 16:16:37 +0530

    test fix

--- src/auth/signin/signUp.jsx
+++ src/auth/signin/signUp.jsx
@@ -1,11 +1,12 @@
 
-import { SignUp } from '@clerk/clerk-react'
+import { SignUp, SignIn } from '@clerk/clerk-react'
 import React from 'react'
 
 function SignUpPage() {
   return (
     <div className='flex justify-center items-center h-screen w-full'>
-      <SignUp />
+      {/* <SignUp /> */}
+      <SignIn />
     </div>
   )
 }
